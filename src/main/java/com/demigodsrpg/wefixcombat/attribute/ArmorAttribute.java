55fcbac523b7db89fa4cd65a9b0ea2e940336cdc
HmmmQuestionMark <ajchauncey@gmail.com>
2015-03-31 17:31:13 -0400

    Fix left overs from the last commit.

--- src/main/java/com/demigodsrpg/wefixcombat/attribute/ArmorAttribute.java
+++ src/main/java/com/demigodsrpg/wefixcombat/attribute/ArmorAttribute.java
@@ -1,5 +1,5 @@
 package com.demigodsrpg.wefixcombat.attribute;
 
-public enum ArmorAttribute implements Attribute<Integer> {
+public enum ArmorAttribute implements Attribute {
     EASE_OF_MOVEMENT, FATIGUE_RATE, DAMAGE_RESISTENCE
 }
--- src/main/java/com/demigodsrpg/wefixcombat/attribute/WeaponAttribute.java
+++ src/main/java/com/demigodsrpg/wefixcombat/attribute/WeaponAttribute.java
@@ -1,5 +1,5 @@
 package com.demigodsrpg.wefixcombat.attribute;
 
-public enum WeaponAttribute implements Attribute<Double> {
+public enum WeaponAttribute implements Attribute {
     DAMAGE, PERCENT_TARGET_SURFACE_AREA, SWITCH_SPEED
 }
